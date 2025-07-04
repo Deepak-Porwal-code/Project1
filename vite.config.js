56356f7c06fbd97536a40625b2223f741c9beedd
Deepak-Porwal-code <Porwal99deepak@gmail.com>
2025-07-04 14:10:49 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/Project1/', // <- your repo name here
-  plugins: [react()],
-})
